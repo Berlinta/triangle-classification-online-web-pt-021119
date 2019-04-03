4cd95e0a780b7c5f6307e6803938af7b4361220b
Nash Brancic <nashfft@gmail.com>
2019-04-03 00:57:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,13 @@ class Triangle
   end
   
   def kind
-    
+    if equilateral == isosceles && isosceles == scalene
+      :equilateral
+    elsif equilateral == isosceles || isosceles == scalene || equilateral == scalene
+      :isosceles
+    else
+      scalene
+    end
   end
 
 end
