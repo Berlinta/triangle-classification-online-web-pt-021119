ee3f47119467246358854d705208360f89a4f79e
Nash Brancic <nashfft@gmail.com>
2019-04-03 00:48:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,6 +3,11 @@ class Triangle
   attr_accessor :equilateral, :isosceles, :scalene
 
   def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
+  
+  def kind
     
+  end
 
 end
